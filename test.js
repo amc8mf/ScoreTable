2d5e177bdd6d82d9ced62bfd853fe395ab553f3a
amc8mf <alexcolegrove93@gmail.com>
2019-07-18 23:06:40 -0400

    remove test file

--- test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const main = require('./app.js');
-const data = require('./data.json');
-
-describe('Basic Mocha String Test', function () {
- it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 4);
-    });
-});
